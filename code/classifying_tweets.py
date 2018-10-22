07405a15a0477ab451b2057e4ddc53a61341724c
França <fmdss@ic.ufal.br>
2018-10-22 03:20:01 -0300

    Add commenters to the script code and notebook's improvements.

--- code/classifying_tweets.py
+++ code/classifying_tweets.py
@@ -1,3 +1,5 @@
+# Importing needed libs:
+
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import *
 from sklearn.dummy import *
@@ -14,33 +16,39 @@ from sklearn.metrics import precision_recall_fscore_support
 import pandas as pd
 import warnings
 
+# Ignoring warnings outputs:
 warnings.filterwarnings("ignore")
 
+# Function to perform a list of models with a list of Vectorizers:
 def perform_models(classifiers, vectorizers, X_train, X_test, y_train, y_test):
     string = ''
+    # For each model classifier for each Vectorizer Algorithm: 
     for classifier in classifiers:
         for vectorizer in vectorizers:
             string += classifier.__class__.__name__ + ' with ' + vectorizer.__class__.__name__
 
-            # train
+            # Train
             vectorize_text_train = vectorizer.fit_transform(X_train)
             classifier.fit(vectorize_text_train, y_train)
-            # evaluating model
+            # Evaluating model
             vectorize_text_test = vectorizer.transform(X_test)
 
             predicteds = classifier.predict(vectorize_text_test)
+            # Getting score metrics
             precision, recall, f1, supp = precision_recall_fscore_support(y_test, predicteds,
                                                                           average='weighted')
             string += '. Has precision: ' + str(precision) + '. recall: ' + str(recall) + ' F1: ' + str(f1) + '\n'
         print(string + '\n')
         string = ''
 
-
+# Reading dataset:
 data = pd.read_csv('../tweets.csv')
+# Dropping useless column:
 data = data.drop(columns='id')
 
+# Splitting randomly our dataset into train/test instances:
 X_train, X_test, y_train, y_test = train_test_split(data['text'], data['type'], test_size=0.3, shuffle=True)
-#Here we could use Cross-validation too -> sklearn.model_selection.KFold
+## Here we could use Cross-validation too -> sklearn.model_selection.KFold
 
 perform_models(
     [
